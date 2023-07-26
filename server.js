df0cae1e4fad993f9c40080054404f8ad2410ff1
Manshi Malik <140046353+manshi8923@users.noreply.github.com>
2023-07-26 09:42:38 +0530

    Update server.js

--- server.js
+++ server.js
@@ -20,11 +20,7 @@ connectDB();
 const app = express();
 
 //middlewares
-app.use(cors(
-  origin:["https://gpt-clone-api.vercel.app"],
-  methods:["POST","GET"],
-  credentials:true
-));
+app.use(cors());
 app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(morgan("dev"));
